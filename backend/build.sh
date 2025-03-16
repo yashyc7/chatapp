25cbf6f25dcb919766882f7ef67b716d80ed0a6a
Yash Chauhan <yashc9411407582@gmail.com>
2025-03-16 15:06:42 +0530

    updated project

--- backend/build.sh
+++ backend/build.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
+# Exit on any error
 set -e  
 
 echo "Setting up virtual environment..."
 python -m venv /opt/venv
 source /opt/venv/bin/activate
 
-echo "Upgrading pip and installing Poetry..."
-pip install --upgrade pip
-pip install poetry==2.1.1
-
-echo "Installing dependencies using Poetry..."
-poetry config virtualenvs.create false
-poetry install --no-dev --no-interaction --no-ansi
-
-echo "Running Django migrations..."
-python manage.py migrate
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+echo "Installing dependencies from requirements.txt..."
+pip install -r requirements.txt
